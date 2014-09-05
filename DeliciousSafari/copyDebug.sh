bffd4d0f124b65c23acf915bf4caf7528e50a107
Doug Richardson <dougie.richardson@gmail.com>
2014-09-05 13:15:33 -0700

    Update copyDebug.sh tool for change to build location... which happened a long time ago, like Xcode 4.

--- DeliciousSafari/copyDebug.sh
+++ DeliciousSafari/copyDebug.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo cp build/Debug/DeliciousSafari.bundle/Contents/MacOS/DeliciousSafari /Library/InputManagers/DeliciousSafari/DeliciousSafari.bundle/Contents/MacOS/DeliciousSafari
-sudo cp -R DXExtenderResources.nib /Library/InputManagers/DeliciousSafari/DeliciousSafari.bundle/Contents/Resources
+set -e
+
+DS_BUILT_BUNDLE_CONTENTS=DerivedData/DeliciousSafari/Build/Products/Debug/DeliciousSafari.bundle/Contents
+DS_DST_CONTENTS=/Library/InputManagers/DeliciousSafari/DeliciousSafari.bundle/Contents
+
+sudo cp $DS_BUILT_BUNDLE_CONTENTS/MacOS/DeliciousSafari $DS_DST_CONTENTS/MacOS/DeliciousSafari
+sudo cp -R $DS_BUILT_BUNDLE_CONTENTS/Resources/*.nib $DS_DST_CONTENTS/Resources
+
+echo OK
